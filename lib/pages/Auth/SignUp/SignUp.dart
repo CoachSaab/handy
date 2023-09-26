285db9aa2a15baca7d809b7458867a65a77872c5
adityayadav3005 <adityayadav300645@gmail.com>
2023-09-26 23:46:53 +0530

    Login Page Base

--- lib/pages/Auth/SignUp/SignUp.dart
+++ lib/pages/Auth/SignUp/SignUp.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:handy/pages/home/HomePage.dart';
-import 'package:get/get.dart';
-
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -9,49 +6,87 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("by your self")),
-        body: Container(
+      body: Stack(alignment: Alignment.center, children: [
+        // Column(children: [
+        //   Expanded(
+        //       flex: 2,
+        //       child: Container(
+        //         color: Colors.blue,
+        //       )),
+        //   Expanded(
+        //       flex: 1,
+        //       child: Container(
+        //         color: Colors.grey,
+        //       )),
+        // ]),
+        Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment(0.5, -0.5),
-              end: Alignment(0.75, 1.5),
-              colors: [
-                Color(0xFFFFEDFF),
-                Color(0xFFB8C0FF),
-                Color(0xFFB8C0FF),
-                Color(0xFF3F4E72)
-              ],
+              begin: Alignment(0.71, -0.71),
+              end: Alignment(-0.71, 0.71),
+              colors: [Color(0xFFFFD6FF), Color(0xFFE7C6FF), Color(0xFFB8C0FF)],
             ),
           ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Login',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.5199999809265137),
-                    fontSize: 36,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-                TextField(),
-                ElevatedButton(onPressed: () {
-                                Get.to(HomePage());
+        ),
 
-                }, child: Text("Submit"))
-              ],
-            ),
+        Positioned(
+          top: MediaQuery.of(context).size.height / 2.5,
+          child: Card(
+            child: SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width - 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 34,
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromRGBO(63, 78, 114, 1)),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                      child: TextField(
+                          decoration: InputDecoration(
+                              suffixIcon: Icon(Icons.account_circle))),
+                    ),
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                      child: TextField(
+                          decoration: InputDecoration(
+                              suffixIcon: Icon(Icons.password))),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                      child: Center(
+                        child: InkWell(
+                            onTap: () {},
+                            child: Card(
+                                color: Colors.blue,
+                                child: Container(
+                                    child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 40.0, vertical: 15),
+                                  child: const Text("Submit",
+                                      style: TextStyle(color: Colors.white)),
+                                )))),
+                      ),
+                    )
+                  ],
+                )),
           ),
-        ));
+        )
+      ]),
+    );
   }
 }
--- lib/utils/Theme/theme.dart
+++ lib/utils/Theme/theme.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Color.fromRGBO(184, 192, 255, 1),
-    foregroundColor: Color.fromRGBO(63, 78, 114, 1)
-  ),
-  iconButtonTheme: IconButtonThemeData(
-      style: ButtonStyle(
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromRGBO(184, 192, 255, 1),
+        foregroundColor: Color.fromRGBO(63, 78, 114, 1)),
+    iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+            iconColor: MaterialStatePropertyAll(Color.fromRGBO(63, 78, 114, 1)),
+            foregroundColor:
+                MaterialStatePropertyAll(Color.fromRGBO(63, 78, 114, 1)))),);
 
-        iconColor: MaterialStatePropertyAll(Color.fromRGBO(63, 78, 114, 1)),
-        foregroundColor: MaterialStatePropertyAll(Color.fromRGBO(63, 78, 114, 1))
-      )
-  )
-);
 
-ThemeData darkTheme = ThemeData(
-  scaffoldBackgroundColor: Colors.blueGrey
-);
+ThemeData darkTheme = ThemeData(scaffoldBackgroundColor: Colors.blueGrey);
